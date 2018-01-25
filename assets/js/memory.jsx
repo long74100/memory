d1581faf2ffaa60f175bcaa5e77f1c986eaa238a
Long <lin.lo@husky.neu.edu>
2018-01-25 01:27:16 -0500

    finished prototype layout

--- assets/js/memory.jsx
+++ assets/js/memory.jsx
@@ -6,84 +6,83 @@ export default function run_memory(root) {
   ReactDOM.render(<MemoryGame />, root);
 }
 
-// App state for MemoryGame is:
-// {
-//    letters: String    // letters used in the game
-//    board: Tile[]      // the current board of the game
-//    completed:Strin    // tiles completed so far
-//    clicks: int        // clicks so far
-// }
-//
-
-class Tile {
-  constructor(value, complete) {
-    this.val = value;
-    this.complete = complete;
-  }
-
-  get value() {
-    return this.val;
-  }
-
-  get complete() {
-    return this.complete;
-  }
-
-  set value(val) {
-    this.val = val;
-  }
-
-}
-
 
 class MemoryGame extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       letters: "ABCDEFGH",
-      board: this.add(),
+      board: [],
       completed: "",
+      activeTile: "",
       clicks: 0,
 
     };
 
   }
 
-  add() {
-    let t = new Tile('A', 2);
-    return [t];
+
+  componentDidMount() {
+
+    function initializeBoard(params) {
+        let letters = params.letters;
+        let fullBoard = letters.repeat(2).split("");
+        let i = 0;
+        while (i < fullBoard.length) {
+          var r = Math.floor(Math.random() * Math.floor(fullBoard.length));
+          let temp = fullBoard[r];
+          fullBoard[r] = fullBoard[i];
+          fullBoard[i] = temp;
+          i++;
+        }
+        console.log(fullBoard);
+        return fullBoard;
+
+    }
+
+    let fullBoard = initializeBoard(this.state);
+    let state = _.extend(this.state, {
+      board: fullBoard,
+    });
+    this.setState(state);
   }
 
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
 
-
-
-  tileLetters() {
+  tiles() {
     return this.state.board;
   }
 
-  render() {
+  completed() {
+    return this.state.completed;
+  }
 
+  render() {
     return (
       <div className ="container">
-      <div>
-        <Tiles root={this} />
-        </div>
+        <Board root={this} />
       </div>
     );
   }
 
 }
 
-function Tiles(params) {
-  let root = params.root;
-  let letters = _.map(root.tileLetters(), (xx, ii) => {
-    return <span key={ii}>{xx.value()}</span>;
-  })
+function Board(props) {
+  let root = props.root;
+  let tiles = root.tiles();
+  let completed = root.completed();
+  //return <div>{tiles.length}</div>
 
-  return (
-    <div>
-      {letters}
-    </div>
-  )
+  let boxes = _.map(tiles, (tile, ii) => {
+    let display = completed.includes(tile) ? tile : " ";
+    return <div className="tile" key={ii}> {display} </div>;
+    });
 
-}
+    return (
+      <div className="board">
+        {boxes}
+      </div>
+    );
+  }
